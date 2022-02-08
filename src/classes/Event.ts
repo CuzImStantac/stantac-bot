c93cb2f2f31bd224d1473c85559bc50888dfee3a
CuzImStantac <61985441+CuzImStantac@users.noreply.github.com>
2022-02-08 19:30:18 +0100

    added enums & new event types

--- src/classes/Event.ts
+++ src/classes/Event.ts
@@ -1,19 +1,46 @@
 import chalk from 'chalk';
 import { ClientEvents } from 'discord.js';
+import { schedule } from 'node-cron';
 import { EventType } from '../types';
 import { Bot } from './';
 
 export abstract class Event {
-  static event: string;
+  static TYPES = EventType;
+
   static type: EventType;
   static emitter?: keyof ClientEvents | string;
   static timeout?: number;
   static interval?: number;
+  static cronExpression?: string;
   static register(bot: Bot): Promise<boolean> {
     return new Promise(async (resolve) => {
       try {
         switch (this.type) {
+          default:
+            bot.logger.error(
+              `Invalid event type "${chalk.redBright(
+                EventType[this.type] ?? this.type
+              )}" for ${chalk.magentaBright(this.name)}.`
+            );
+            break;
           case EventType.PROCESS:
+            if (this?.emitter)
+              process.on(this.emitter, (...args) => {
+                bot.logger.debug(
+                  `Process triggered the event "${chalk.yellowBright(
+                    this.name
+                  )}".`
+                );
+                try {
+                  this.execute(bot, ...args);
+                } catch (e: Error | any) {
+                  bot.logger.error(
+                    `Unable to execute ${chalk.redBright(this.name)}! Error: ${
+                      e.message
+                    }`
+                  );
+                }
+              });
             break;
           case EventType.DISCORD:
             if (this?.emitter)
@@ -35,8 +62,59 @@ export abstract class Event {
               });
             break;
           case EventType.INTERVAL:
+            if (this.interval)
+              setInterval(() => {
+                bot.logger.debug(
+                  `Interval triggered the event "${chalk.yellowBright(
+                    this.name
+                  )}".`
+                );
+                try {
+                  this.execute(bot);
+                } catch (e: Error | any) {
+                  bot.logger.error(
+                    `Unable to execute ${chalk.redBright(this.name)}! Error: ${
+                      e.message
+                    }`
+                  );
+                }
+              }, this.interval);
             break;
           case EventType.TIMEOUT:
+            if (this.timeout)
+              setTimeout(() => {
+                bot.logger.debug(
+                  `Timeout triggered the event "${chalk.yellowBright(
+                    this.name
+                  )}".`
+                );
+                try {
+                  this.execute(bot);
+                } catch (e: Error | any) {
+                  bot.logger.error(
+                    `Unable to execute ${chalk.redBright(this.name)}! Error: ${
+                      e.message
+                    }`
+                  );
+                }
+              }, this.timeout);
+            break;
+          case EventType.CRON:
+            if (this.cronExpression)
+              schedule(this.cronExpression, () => {
+                bot.logger.debug(
+                  `Cron triggered the event "${chalk.yellowBright(this.name)}".`
+                );
+                try {
+                  this.execute(bot);
+                } catch (e: Error | any) {
+                  bot.logger.error(
+                    `Unable to execute ${chalk.redBright(this.name)}! Error: ${
+                      e.message
+                    }`
+                  );
+                }
+              });
             break;
         }
         return resolve(true);
@@ -46,14 +124,11 @@ export abstract class Event {
     });
   }
 
-  static getName() {
-    Object.getPrototypeOf(this).constructor.name;
-  }
-  static getType() {
+  static getType(): string | undefined {
     return EventType[this.type];
   }
 
-  static execute(bot: Bot, ...args: any[]): void {
+  static async execute(bot: Bot, ...args: any[]): Promise<void> {
     bot.logger.event(
       this,
       `Event not implemented! Recived ${chalk.yellowBright(
